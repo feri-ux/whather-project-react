2470d0e742f4049990d319b6d5949a7a98f97c3c
fereshteh <114668669+feri-ux@users.noreply.github.com>
2023-04-16 22:17:18 +0430

    added new component WeatherInfo

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,14 @@
 import React,{useState} from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-import { faCloudBolt } from "@fortawesome/free-solid-svg-icons";
-import 'bootstrap/dist/css/bootstrap.css';
-import FormattedDate from "./FormattedDate";
-import FormattedTime from "./FormattedTime";
+import WeatherInfo from "./WeatherInfo";
 
 
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather(props) {
-  
   const[weatherData, setWeatherData]=useState({ready:false})
+  const[city, setCity]=useState(props.defaultCity)
+
   function handleResponse(response){
     setWeatherData({
     ready:true,
@@ -28,10 +23,25 @@ export default function Weather(props) {
 
   }
 
+  function Search(){
+    const apiKey="1a6432c5ca7b6f9b0bee45c98d54ea71"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    Search()
+  }
+  
+  function handleCityChange(event){
+    setCity(event.target.value)
+  }
+
   if (weatherData.ready) {
     return (
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-8">
               <div className="form-group">
@@ -39,6 +49,7 @@ export default function Weather(props) {
                   type="text"
                   placeholder="Enter a city..."
                   className="form-control"
+                  onChange={handleCityChange}
                 />
               </div>
             </div>
@@ -51,147 +62,11 @@ export default function Weather(props) {
           </div>
         </form>
         <br />
-  
-        <div className="row">
-          <div className="col-7">
-            <div className="temperature-large">
-              <span id="degree">{Math.round(weatherData.temperature)} </span>
-              <span className="units">
-                <a href="#" className="celcius">
-                  °C{" "}
-                </a>
-                <span className="verticalLine">|</span>
-                <a href="#" className="fahrenheit">
-                  °F
-                </a>
-              </span>
-            </div>
-          </div>
-          <div className="col-5">
-            <div className="city-name">{weatherData.city}</div>
-          </div>
-        </div>
-        <br />
-  
-        <div className="row">
-          <div className="col-7">
-            <div>
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="weather-icon"
-              />
-            </div>
-          </div>
-          <div className="col-5">
-            <div className="extrainfo">Humidity: {weatherData.humidity}%</div>
-            <div className="extrainfo">Wind: {Math.round(weatherData.wind)} Km/h</div>
-          </div>
-        </div>
-  
-        <div className="row">
-          <div className="col-5">
-            <h2 className="text-capitalize">{weatherData.description}</h2>
-          </div>
-          <div class="col-7">
-            <div class="enjoy-weather">Enjoy this beautiful day!</div>
-          </div>
-        </div>
-        <br />
-  
-        <div className="row">
-          <div className="col-5 time">
-            <div>
-              <FontAwesomeIcon icon={faClock} />
-              <span id="current-time"> <FormattedTime date={weatherData.date}/></span>
-            </div>
-          </div>
-          <div className="col-7">
-            <div className="dates">
-              <FontAwesomeIcon icon={faCalendar} />
-  
-              <i className="fa-regular fa-calendar"><FormattedDate date={weatherData.date}/> </i>
-              <span id="date"></span>
-            </div>
-          </div>
-        </div>
-        <hr align="left" color="#E4ECEF" className="hr" />
-  
-  
-        <div className="row">
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Fri</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25° </span>
-              <span className="forecast-temperature-min"> 17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Sut</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Sun</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-  
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Mon</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Tue</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-        </div>
-        <hr  align="left"  className="half-hr" />
-  
-        <div className="footer">
-          <a href="https://github.com/feri-ux/whather-project-react" target="_blank" rel="noreferrer">
-            Open-source code </a
-          >
-           by Asieh Kalantar
-        </div>
+        <WeatherInfo data={weatherData}/>
       </div>
     );
   } else {
-    const apiKey="1a6432c5ca7b6f9b0bee45c98d54ea71"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse)
-    
+    Search()
     return "loading..."
-    
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,149 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faCloudBolt } from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.css';
+import FormattedDate from "./FormattedDate";
+import FormattedTime from "./FormattedTime";
+
+export default function WeatherInfo(props){
+    return(
+        <div className="WeatherInfo">
+<div className="row">
+          <div className="col-7">
+            <div className="temperature-large">
+              <span id="degree">{Math.round(props.data.temperature)} </span>
+              <span className="units">
+                <a href="#" className="celcius">
+                  °C{" "}
+                </a>
+                <span className="verticalLine">|</span>
+                <a href="#" className="fahrenheit">
+                  °F
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="city-name">{props.data.city}</div>
+          </div>
+        </div>
+        <br />
+  
+        <div className="row">
+          <div className="col-7">
+            <div>
+              <img
+                src={props.data.imgUrl}
+                alt={props.data.description}
+                className="weather-icon"
+              />
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="extrainfo">Humidity: {props.data.humidity}%</div>
+            <div className="extrainfo">Wind: {Math.round(props.data.wind)} Km/h</div>
+          </div>
+        </div>
+  
+        <div className="row">
+          <div className="col-5">
+            <h2 className="text-capitalize">{props.data.description}</h2>
+          </div>
+          <div class="col-7">
+            <div class="enjoy-weather">Enjoy this beautiful day!</div>
+          </div>
+        </div>
+        <br />
+  
+        <div className="row">
+          <div className="col-6 time">
+            <div className="row">
+          <div className="col-2">
+              <FontAwesomeIcon icon={faClock} />
+              </div>
+          <div className="col-2 ">
+              <span > <FormattedTime date={props.data.date}/></span>
+            </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="dates">
+              <span><FontAwesomeIcon icon={faCalendar} /> </span>
+              <span><FormattedDate date={props.data.date}/></span>
+            </div>
+          </div>
+        </div>
+        <hr align="left" color="#E4ECEF" className="hr" />
+  
+  
+        <div className="row">
+          <div className="col-sm-2 weather-button">
+            <div className="forecast-day">Fri</div>
+            <br />
+            <div className="forecast-icon">
+            <FontAwesomeIcon icon={faCloudBolt} />
+            </div>
+            <div className="forecast-temperature">
+              <span>25° </span>
+              <span className="forecast-temperature-min"> 17°</span>
+            </div>
+          </div>
+          <div className="col-sm-2 weather-button">
+            <div className="forecast-day">Sut</div>
+            <br />
+            <div className="forecast-icon">
+            <FontAwesomeIcon icon={faCloudBolt} />
+            </div>
+            <div className="forecast-temperature">
+              <span>25°</span>
+              <span className="forecast-temperature-min">17°</span>
+            </div>
+          </div>
+          <div className="col-sm-2 weather-button">
+            <div className="forecast-day">Sun</div>
+            <br />
+            <div className="forecast-icon">
+            <FontAwesomeIcon icon={faCloudBolt} />
+  
+            </div>
+            <div className="forecast-temperature">
+              <span>25°</span>
+              <span className="forecast-temperature-min">17°</span>
+            </div>
+          </div>
+          <div className="col-sm-2 weather-button">
+            <div className="forecast-day">Mon</div>
+            <br />
+            <div className="forecast-icon">
+            <FontAwesomeIcon icon={faCloudBolt} />
+            </div>
+            <div className="forecast-temperature">
+              <span>25°</span>
+              <span className="forecast-temperature-min">17°</span>
+            </div>
+          </div>
+          <div className="col-sm-2 weather-button">
+            <div className="forecast-day">Tue</div>
+            <br />
+            <div className="forecast-icon">
+            <FontAwesomeIcon icon={faCloudBolt} />
+            </div>
+            <div className="forecast-temperature">
+              <span>25°</span>
+              <span className="forecast-temperature-min">17°</span>
+            </div>
+          </div>
+        </div>
+        <hr  align="left"  className="half-hr" />
+  
+        <div className="footer">
+          <a href="https://github.com/feri-ux/whather-project-react" target="_blank" rel="noreferrer">
+            Open-source code </a
+          >
+           by Asieh Kalantar
+        </div>
+        </div>
+    )
+}
