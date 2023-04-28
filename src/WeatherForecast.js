d01eea36305bf0ece6d901006af10116ea148dec
fereshteh <114668669+feri-ux@users.noreply.github.com>
2023-04-28 17:13:35 +0430

    refactored the code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,89 +1,55 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudBolt } from "@fortawesome/free-solid-svg-icons";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect,  } from "react"
 import "./WeatherForecast.css"
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 
 export default function WeatherForecast(props){
-    function handleResponse(response){
-        console.log(response.data)
-    }
-    console.log(props)
+  let [loaded, setLoaded]=useState(false)
+  let [forecast, setForecast]=useState(null)
 
-    let apiKey="e450bc345a80a08ada69fd5c714d871d"
-    let latitude=props.coordinates.lat
-    let longitude=props.coordinates.lon
-    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(handleResponse)
-    return(
-    <div className="WeatherForecast">
-         <div className="row">
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Fri</div>
-            <br />
-            <div className="forecast-icon">
-            <WeatherIcon code="01d"  size={40} color="black"/>
+  useEffect(()=>{
+    setLoaded(false);
+  }, [props.coordinates]);
+
+
+    function handleResponse(response){
+        setForecast(response.data.daily)
+        setLoaded(true)
+    }
+    
+    if (loaded) {
+      return(
+        <div className="WeatherForecast">
+             <div className="row">
+              {forecast.map(function(dailyForecast, index){
+                if (index < 5) {
+                  return(
+                    <div className="col-sm-2 weather-button" key={index}>
+                      <WeatherForecastDay data={dailyForecast}/>
+                    </div>
+                  );
+                }else{
+                  return null
+                }
+              })}
+            <hr  align="left"  className="half-hr" />
+            <div className="footer">
+
+              <a href="https://github.com/feri-ux/whather-project-react" target="_blank" rel="noreferrer">
+                Open-source code </a
+              >
+               by Asieh Kalantar
             </div>
-            <div className="forecast-temperature">
-              <span>25° </span>
-              <span className="forecast-temperature-min"> 17°</span>
             </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Sut</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
             </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Sun</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-  
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Mon</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-          <div className="col-sm-2 weather-button">
-            <div className="forecast-day">Tue</div>
-            <br />
-            <div className="forecast-icon">
-            <FontAwesomeIcon icon={faCloudBolt} />
-            </div>
-            <div className="forecast-temperature">
-              <span>25°</span>
-              <span className="forecast-temperature-min">17°</span>
-            </div>
-          </div>
-        </div>
-        <hr  align="left"  className="half-hr" />
-  
-        <div className="footer">
-          <a href="https://github.com/feri-ux/whather-project-react" target="_blank" rel="noreferrer">
-            Open-source code </a
-          >
-           by Asieh Kalantar
-        </div>
-        </div>)
+            )
+    } else {
+      let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
+      let latitude=props.coordinates.lat
+      let longitude=props.coordinates.lon
+      let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      axios.get(apiUrl).then(handleResponse)
+      return null;
+    }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,38 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+
+
+export default function WeatherForecastDay(props){
+
+    function maxTemperature(){
+        let temperature=Math.round(props.data.temp.max)
+        return`${temperature}°`
+    }
+
+    function minTemperature(){
+        let temperature=Math.round(props.data.temp.min)
+        return`${temperature}°`
+    }
+
+    function day(){
+        let date= new Date(props.data.dt * 1000);
+        let day= date.getDay();
+        let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat",]
+        return days[day];
+    }
+
+    return(
+           <div className="WeatherForecastDay">
+                <div className="forecast-day">{day()}</div>
+                <br />
+                <div className="forecast-icon">
+                <WeatherIcon code={props.data.weather[0].icon}  size={40} color="black"/>
+                </div>
+                <div className="forecast-temperature">
+                  <span> {maxTemperature()} </span>
+                  <span className="forecast-temperature-min"> {minTemperature()}</span>
+                </div>
+          </div>
+          );
+}
